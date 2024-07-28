41412f3cdd264ddf31e754206461686b7e97432c
Noah Too <krivahtoo@gmail.com>
2024-07-28 22:04:45 +0300

    Fix Task 0. Hello Flask formatting

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,6 +7,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
@@ -15,6 +16,6 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
