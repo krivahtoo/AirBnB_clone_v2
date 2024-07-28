1ddfa8d260397b047ea70f48b8527b3dcb0d28fd
Noah Too <krivahtoo@gmail.com>
2024-07-28 23:13:21 +0300

    Update Task 6. Odd or even? and add .gitignore

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -66,7 +66,7 @@ def number_odd_or_even(n):
         H1 tag: “Number: n is even|odd” inside the tag BODY
     """
     parity = "even" if n % 2 == 0 else "odd"
-    return render_template('number_odd_or_even.html', n=n, parity=parity)
+    return render_template('6-number_odd_or_even.html', n=n, parity=parity)
 
 
 if __name__ == '__main__':
