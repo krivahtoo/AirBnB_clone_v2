7d2322e66116e035e2c5c2d6f90cc9d6349a1b3b
Noah Too <krivahtoo@gmail.com>
2024-07-28 22:23:04 +0300

    Add Task 2. C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Task 1. HBNB
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Root endpoint
+    Return: Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    hbnb endpoint
+    Return: HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Return: “C ” followed by the value of the text variable
+        (replace underscore _ symbols with a space )
+    """
+    return "C " + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
