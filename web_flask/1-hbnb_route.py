12a06b135963f58bc02f53afeec8f2b4da52d7dd
Noah Too <krivahtoo@gmail.com>
2024-07-28 22:11:53 +0300

    Add Task 1. HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Task 1. HBNB
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Root endpoint
+    Return: Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    hbnb endpoint
+    Return: HBNB
+    """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
